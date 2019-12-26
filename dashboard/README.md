67e18b42be4f9d076a6a416f87a8834817ce428d
Lars Ericsson <larserics@gmail.com>
2019-12-26 11:42:54 +0100

    Show portfolio values

---
+++ dashboard/README.md
@@ -0,0 +1,16 @@
+# Trading Bot Challenge Dashboard
+
+## Setup
+```
+npm install
+```
+
+## Development
+```
+npm run dev
+```
+
+## Deploy
+```
+npm run deploy
+```
---
+++ dashboard/src/js/index.js
@@ -0,0 +1,19 @@
+const firebase = require("firebase");
+
+firebase.initializeApp({
+  apiKey: '### FIREBASE API KEY ###',
+  authDomain: '### FIREBASE AUTH DOMAIN ###',
+  projectId: 'trading-bot-challenge'
+});
+  
+const db = firebase.firestore();
+const portfolios = document.querySelector('.js-portfolios');
+
+db.collection("portfolios").get().then((querySnapshot) => {
+  querySnapshot.forEach((doc) => {
+    console.log(`${doc.id} => ${doc.data()}`);
+    let portfolioNode = document.createElement('p');
+    portfolioNode.innerHTML = `${doc.id} - ${doc.data().value}`;
+    portfolios.appendChild(portfolioNode);
+  });
+});
