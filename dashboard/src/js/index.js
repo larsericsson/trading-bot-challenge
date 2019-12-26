65dd27a423cf6c0b5f299bc1a7320dad69b262de
John Häggkvist Forne <john.haggkvist@gmail.com>
2019-12-26 23:10:52 +0100

    Hello world

--- dashboard/src/js/index.js
+++ dashboard/src/js/index.js
@@ -5,13 +5,13 @@ firebase.initializeApp({
   authDomain: '### FIREBASE AUTH DOMAIN ###',
   projectId: 'trading-bot-challenge'
 });
-  
+
 const db = firebase.firestore();
 const portfolios = document.querySelector('.js-portfolios');
 
 db.collection("portfolios").get().then((querySnapshot) => {
   querySnapshot.forEach((doc) => {
-    console.log(`${doc.id} => ${doc.data()}`);
+    console.log(doc.id, doc.data());
     let portfolioNode = document.createElement('p');
     portfolioNode.innerHTML = `${doc.id} - ${doc.data().value}`;
     portfolios.appendChild(portfolioNode);
