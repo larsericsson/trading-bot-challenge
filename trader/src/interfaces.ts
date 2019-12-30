87227a6bdd88e264fd7090e87ab7fcdf0cb73066
Lars Ericsson <larserics@gmail.com>
2019-12-30 18:32:15 +0100

    Add interface for strategies

--- trader/src/interfaces.ts
+++ trader/src/interfaces.ts
@@ -1,5 +1,13 @@
 export interface Config {
-  username: string;
-  password: string;
-  totpSecret: string;
+  username: string
+  password: string
+  totpSecret: string
+}
+
+export interface IQuote {
+  name: string
+}
+
+export interface IStrategy {
+  onQuote(quote: IQuote): void
 }
--- trader/src/strategies/active/dummy.ts
+++ trader/src/strategies/active/dummy.ts
@@ -1,5 +1,7 @@
-const DummyStrategy = {
-  onQuote: function(quote) {
+import { IStrategy, IQuote } from '../../interfaces'
+
+const DummyStrategy: IStrategy = {
+  onQuote: function(quote: IQuote) {
     console.log('dummy got quote')
     const buy: boolean = true
     return buy
