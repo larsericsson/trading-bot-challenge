c1e3c40b6b4709d93ffc9d6f5a3ea6183d4f1711
John Häggkvist Forne <john.haggkvist@gmail.com>
2020-01-15 10:55:00 +0100

    Added logging

--- trader/src/app.ts
+++ trader/src/app.ts
@@ -18,14 +18,25 @@ async function setup() {
   Strategies.initialize()
 
   recordPortfolioTask = scheduleJob('recordPortfolio', '*/5 9-18 * * 1-5', async () => {
-    const portfolio = await AvanzaService.getPortfolio(avanzaClient, config.avanza.accountId)
-    FirestoreService.recordPortfolio(firestoreClient, config.portfolioId, portfolio)
+    if (avanzaClient.isAuthenticated) {
+      const portfolio = await AvanzaService.getPortfolio(avanzaClient, config.avanza.accountId)
+      FirestoreService.recordPortfolio(firestoreClient, config.portfolioId, portfolio)
+    } else {
+      console.error(`Avanza client not authed!`);
+    }
   })
 
   console.log('Application started')
   console.group()
   console.log(`Portfolio: ${config.portfolioId}`)
   console.log(`Avanza: ${avanzaClient.isAuthenticated ? 'authed' : 'not authed'}`)
+  if (avanzaClient.isAuthenticated) {
+    console.group()
+    let portfolio = await AvanzaService.getPortfolio(avanzaClient, config.avanza.accountId)
+    console.log(`Balance: ${portfolio.totalBalance}`)
+    console.log(`Profit:  ${portfolio.totalProfit}`)
+    console.groupEnd()
+  }
   console.log(
     `Scheduled next invocation of ${
       recordPortfolioTask.name
